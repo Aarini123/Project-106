4d447c8a3db83972e79a5a3311950657a0c412f2
Aarini123 <77624143+Aarini123@users.noreply.github.com>
2021-05-16 15:35:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+ var p1="";
+ var p2="";
+
+Webcam.set({
+    width:350,
+    height:330,
+    image_format:"png",
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("snap").innerHTML="<img id='captured_image' src='"+data_uri+"'>";
+    });
+}
+
+console.log("ml5.version",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/utSP-bhh7/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("Model_loaded!");
+}
